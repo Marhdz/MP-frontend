f818bc413aae11488708e22ce774ceb3020c8b27
Michelle Romero <33272156+Marhdz@users.noreply.github.com>
2021-11-12 21:09:35 -0600

    change to prod

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NODE_ENV !== "production" ? "http://localhost:8080/" : "https://api-sdpm-simulator.herokuapp.com/",
+  baseURL: process.env.NODE_ENV !== "production" ? "https://simulador-memoria.herokuapp.com" : "https://simulador-memoria.herokuapp.com",
   headers: {
     "Access-Control-Allow-Origin" : "*",
     "Access-Control-Allow-Methods":"GET,PUT,POST,DELETE,PATCH,OPTIONS",
