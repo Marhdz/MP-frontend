ed5ccc07bbed8dfc1b9c8b70500983e0af3270b6
Michelle Romero <33272156+Marhdz@users.noreply.github.com>
2021-11-13 12:54:37 -0600

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 require("dotenv").config();
 
-//ReactDOM.render( document.getElementById("root"));
+ReactDOM.render(<SDPMRootPage/>, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
